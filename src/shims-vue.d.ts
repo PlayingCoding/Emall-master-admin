e26d6479fa083b648c24fd7005354d43cc84a921
littleLane <lzz857183384@163.com>
2020-07-22 09:27:10 +0800

    feat: code format & types

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,15 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+// 声明全局的 window ，不然使用 window.XX 时会报错
+declare var window: Window & typeof globalThis
+declare var document: Document
+
+declare module 'element-ui/lib/transitions/collapse-transition'
+declare module 'element-ui'
+
+declare module '*.less' {
+  const less: any
+  export default less
+}
